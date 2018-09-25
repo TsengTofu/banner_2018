cc764cbec78da9ed57f3a80dc0c1c9ab4bcc23a0
TsengTofu <tsengbatty@gmail.com>
2018-09-25 08:18:26 +0800

    Add files via upload

---
+++ html_0310/js/gotop.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+
+
+$(function(){
+	
+	$("#goTop").click(function(){
+
+		$("html,body").animate({scrollTop:0},900);
+
+		//$("html,body").animate({scrollTop:0},900,"easeOutBounce");
+
+		return false;
+
+	});
+
+});
+
